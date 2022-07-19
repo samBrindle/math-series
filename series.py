d2fed5c0b3b96c66382a3f50e94ff9895d206223
Sam Brindle <brindlejsam@gmail.com>
2022-07-19 16:33:45 -0700

    added lucas function

--- series.py
+++ series.py
@@ -10,3 +10,16 @@ def fibonacci(n):
         return 1
 
     return fibonacci(n-1) + fibonacci(n-2)
+
+def lucas(n):
+    """
+    Returns number at index n of the lucas sequence.
+    :param n:
+    :return:
+    """
+    if n <= 0:
+        return 2
+    if n == 1:
+        return 1
+
+    return lucas(n-1) + lucas(n-2)
