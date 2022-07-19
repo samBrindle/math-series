deaec2741227e732229bbe52583b966d20676eb0
Sam Brindle <brindlejsam@gmail.com>
2022-07-19 16:31:03 -0700

    added the fibonacci code and got it working

--- series.py
+++ series.py
@@ -1,1 +1,13 @@
 def fibonacci(n):
+    """
+    Returns number at index n of the fibonacci sequence.
+    :param n:
+    :return:
+    """
+    if n <= 0:
+        return 0
+    if n == 1 or n == 2:
+        return 1
+
+    return fibonacci(n-1) + fibonacci(n-2)
+
--- test_series.py
+++ test_series.py
@@ -1,4 +1,4 @@
-from series import fibonacci
+from series import fibonacci, lucas, sum_series
 
 def test_fibonacci_zero():
     actual = fibonacci(0)
