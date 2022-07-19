362c033f14938a8a5647724685b067b116adf329
Sam Brindle <brindlejsam@gmail.com>
2022-07-19 16:33:08 -0700

    added lucas tests

--- series.py
+++ series.py
@@ -10,4 +10,3 @@ def fibonacci(n):
         return 1
 
     return fibonacci(n-1) + fibonacci(n-2)
-
--- test_series.py
+++ test_series.py
@@ -44,3 +44,48 @@ def test_fibonacci_eight():
     actual = fibonacci(8)
     expected = 21
     assert actual == expected
+
+def test_lucas_zero():
+    actual = lucas(0)
+    expected = 2
+    assert actual == expected
+
+def test_lucas_one():
+    actual = lucas(1)
+    expected = 1
+    assert actual == expected
+
+def test_lucas_two():
+    actual = lucas(2)
+    expected = 3
+    assert actual == expected
+
+def test_lucas_three():
+    actual = lucas(3)
+    expected = 4
+    assert actual == expected
+
+def test_lucas_four():
+    actual = lucas(4)
+    expected = 7
+    assert actual == expected
+
+def test_lucas_five():
+    actual = lucas(5)
+    expected = 11
+    assert actual == expected
+
+def test_lucas_six():
+    actual = lucas(6)
+    expected = 18
+    assert actual == expected
+
+def test_lucas_seven():
+    actual = lucas(7)
+    expected = 29
+    assert actual == expected
+
+def test_lucas_eight():
+    actual = lucas(8)
+    expected = 47
+    assert actual == expected
